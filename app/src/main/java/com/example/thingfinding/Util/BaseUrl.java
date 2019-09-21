98712d121a989371cca388065c101a79a9462401
lee <1710657239@qq.com>
2019-09-21 16:39:00 +0800

    增加了维修服务类

---
+++ app/src/main/java/com/example/thingfinding/Util/BaseUrl.java
@@ -0,0 +1,8 @@
+package com.example.thingfinding.Util;
+
+/**
+ * Created by Lee on 2019/9/20
+ */
+public class BaseUrl {
+    public static final String BASE_URL = "http://192.168.2.100:8080/customerDemand";
+}
